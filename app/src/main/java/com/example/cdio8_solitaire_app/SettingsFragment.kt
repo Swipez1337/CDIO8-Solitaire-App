383d12b53631ff12a0965832592316fdeff59c53
Swipez1337 <frederiklundsbjerg@hotmail.com>
2022-04-20 15:41:36 +0200

    settings section and navigation, changes to first fragment etc.

--- app/src/main/java/com/example/cdio8_solitaire_app/SettingsFragment.kt
+++ app/src/main/java/com/example/cdio8_solitaire_app/SettingsFragment.kt
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.cdio8_solitaire_app.databinding.FragmentFirstBinding
+import com.example.cdio8_solitaire_app.databinding.FragmentSecondBinding
+import com.example.cdio8_solitaire_app.databinding.FragmentSettingsBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,12 +33,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-
-        binding.imageView1.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SettingsFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SettingsFragment_to_FirstFragment)
         }
     }
 
