e12d7f97e55ff6d093799ff011059ccf92efbe02
SAA-DK <sebastian_almfort@hotmail.com>
2022-06-03 14:57:56 +0200

    Have tried to setup camera. Has started debuggin

--- app/src/main/java/com/example/cdio8_solitaire_app/MainActivity.kt
+++ app/src/main/java/com/example/cdio8_solitaire_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.cdio8_solitaire_app
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,18 +13,52 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.annotation.Nullable
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.cdio8_solitaire_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Initialize Variable
+    //ImageView imageView;
+    //Button btOpen;
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+        //Assign Variable
+        var imageView = findViewById(R.id.image_view);
+        var btOpen = findViewById(R.id.bt_open)
+
+        //Request for Camera Permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            val new = null
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                    Manifest.permission.CAMERA
+                },
+                100)
+        }
+
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open Camera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        })
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root);
+
 
         setSupportActionBar(binding.toolbar)
 
@@ -31,6 +69,16 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+    }
+
+    @Override
+    protected void OnActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if (requestCode == 100){
+            //Get Capture Image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            //Set Capture Image to ImageView
+            imageView.setImageBitmap(captureImage)
         }
     }
 
@@ -55,4 +103,12 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+}
+
+private operator fun Any.invoke(value: Any): Any {
+
+}
+
+private infix fun Nothing?.String(any: Any): Array<String> {
+
 }
