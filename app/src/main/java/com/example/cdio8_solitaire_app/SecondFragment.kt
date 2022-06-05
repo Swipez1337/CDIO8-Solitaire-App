575e1b6ba1d6c2e8ea5ae5e65d8df9afa1e2309a
Freja Jensen <frejaj9@gmail.com>
2022-06-05 21:32:11 +0200

    Implemented camera function (DOESN'T WORK)

--- app/src/main/java/com/example/cdio8_solitaire_app/SecondFragment.kt
+++ app/src/main/java/com/example/cdio8_solitaire_app/SecondFragment.kt
@@ -1,12 +1,21 @@
 package com.example.cdio8_solitaire_app
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.cdio8_solitaire_app.databinding.FragmentSecondBinding
+import kotlin.math.log
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,16 +23,17 @@ import com.example.cdio8_solitaire_app.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private val REQUEST_CODE = 1
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -31,15 +41,29 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent,REQUEST_CODE)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(context,"camera is not working",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imageView2.setImageBitmap(imageBitmap)
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
 }
